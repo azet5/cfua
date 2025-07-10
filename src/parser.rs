e5cf7a0248844a28613c7f166983e7e8f5dc03b3
azet5 <51453773+dyzio6684@users.noreply.github.com>
2025-07-10 18:46:13 +0200

    Implement parser

--- src/parser.rs
+++ src/parser.rs
@@ -2,23 +2,31 @@ use std::str::Chars;
 
 use crate::{cfua::CfuaType, Cfua};
 
+type CfuaError = ();
+
 enum State {
     Reading,
     Key,
     Separator,
-    ValueString,
-    ValueNumber,
-    ValueBool,
-    ValueArray,
+    Value,
     SectionName,
     Comment,
-    EndOfData,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum ValueType {
+    Number,
+    Float,
+    String,
+    Boolean,
+    Array(Box<ValueType>),
 }
 
 pub struct ParserData {
     input: String,
     key_buffer: String,
-    value_buffer: CfuaType,
+    value_buffer: String,
+    value_type: ValueType,
     state: State,
     data: Cfua,
 }
@@ -28,61 +36,145 @@ impl ParserData {
         Self {
             input,
             key_buffer: String::with_capacity(256),
-            // its value doesn't matter as it will be replaced before using
-            value_buffer: CfuaType::Integer(0),
+            value_buffer: String::with_capacity(256),
+            value_type: ValueType::Number,
             state: State::Reading,
             data: Cfua::create(),
         }
     }
 
-    fn advance(&mut self, chars: &mut Chars<'_>) {
-        
+    fn push_value(&mut self) -> Result<(), CfuaError> {
+        if self.value_type == ValueType::String {
+            self.data.write_string(self.key_buffer.clone(), self.value_buffer.strip_prefix('\'').unwrap().to_string());
+        } else {
+            if self.value_buffer == "true" {
+                self.data.write_bool(self.key_buffer.clone(), true);
+            } else if self.value_buffer == "false" {
+                self.data.write_bool(self.key_buffer.clone(), false);
+            } else if self.value_buffer == "nan" {
+                self.data.write_float(self.key_buffer.clone(), f64::NAN);
+            } else if self.value_buffer == "inf" {
+                self.data.write_float(self.key_buffer.clone(), f64::INFINITY);
+            } else if self.value_buffer == "-inf" {
+                self.data.write_float(self.key_buffer.clone(), f64::NEG_INFINITY);
+            } else {
+                return Err(());
+            }
+        }
+
+        self.state = State::Reading;
+        Ok(())
     }
 
-    fn section_char(&mut self, char: char) {
+    fn section_char(&mut self, char: char) -> Result<(), CfuaError> {
+        match char {
+            '\n' => {
+                self.state = State::Reading;
+                self.data.write_section(self.key_buffer.clone());
+            },
+            'a'..'z' => self.key_buffer.push(char),
+            '-' => {
+                if self.key_buffer.len() > 1 {
+                    self.key_buffer.push(char);
+                } else {
+                    return Err(());
+                }
+            },
+            _ => return Err(()),
+        }
+
+        Ok(())
+    }
+
+    fn key_char(&mut self, char: char) -> Result<(), CfuaError> {
+        match char {
+            ':' => self.state = State::Separator,
+            'a'..'z' => self.key_buffer.push(char),
+            '-' => if self.key_buffer.len() > 1 {
+                self.key_buffer.push(char);
+            } else {
+                return Err(());
+            },
+            ' ' => if !self.key_buffer.is_empty() {
+                return Err(());
+            },
+            _ => return Err(()),
+        }
+
+        Ok(())
+    }
+    
+    fn separator_char(&mut self, char: char) -> Result<(), CfuaError> {
+        if char == ' ' {
+            Ok(())
+        } else if char.is_ascii_graphic() {
+            self.state = State::Value;
+            self.value_buffer.clear();
+            return self.value_char(char);
+        } else {
+            Err(())
+        }
+    }
+
+    fn value_char(&mut self, char: char) -> Result<(), CfuaError> {
+        if self.value_buffer.len() == 0 {
+            match char {
+                '\'' => self.value_type = ValueType::String,
+                '-' |
+                'b' |
+                'h' |
+                'o' |
+                '0'..'9' => self.value_type = ValueType::Number,
+                '\n' => return Err(()),
+                _ => {},
+            }
+        } else {
+            if char == '\n' {
+                return self.push_value();
+            }
+        }
+
+        self.value_buffer.push(char);
+        Ok(())
+    }
+
+    fn comment_char(&mut self, char: char) -> Result<(), CfuaError> {
         if char == '\n' {
             self.state = State::Reading;
-            // self.data.write_section(self.key_buffer);
-        } else {
-            self.key_buffer.push(char);
         }
+
+        Ok(())
     }
 
-    fn comment_char(&mut self, char: char) {
-        if char == '\n' {
-            self.state = State::Reading
-        }
-    }
-
-    fn basic_char(&mut self, char: char) {
+    fn basic_char(&mut self, char: char) -> Result<(), CfuaError> {
         match char {
             '%' => self.state = State::Comment,
             '@' => self.state = State::SectionName,
-            _ => {},
+            'a'..'z' => self.state = State::Key,
+            '\n' => {},
+            _ => return Err(()),
         }
+
+        Ok(())
     }
 
-    fn read_char(&mut self, char: char) {
+    fn read_char(&mut self, char: char) -> Result<(), CfuaError> {
         match self.state {
             State::Reading => self.basic_char(char),
-            State::Key => todo!(),
-            State::Separator => todo!(),
-            State::ValueString => todo!(),
-            State::ValueNumber => todo!(),
-            State::ValueBool => todo!(),
-            State::ValueArray => todo!(),
+            State::Key => self.key_char(char),
+            State::Separator => self.separator_char(char),
+            State::Value => self.value_char(char),
             State::SectionName => self.section_char(char),
             State::Comment => self.comment_char(char),
-            State::EndOfData => todo!(),
         }
     }
 
-    pub fn parse(&mut self) -> Result<Cfua, ()> {
+    pub fn parse(&mut self) -> Result<Cfua, CfuaError> {
         let input = self.input.clone();
         let mut chars = input.chars();
 
         while let Some(char) = chars.next() {
-            self.read_char(char);
+            self.read_char(char)?;
         }
 
         // loop {
